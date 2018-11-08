7c8b01af21aefb7b972aee8bb961cab1e5501f1c
kylegoines <kyle.goines@gmail.com>
2018-11-08 12:02:03 -0500

    init test :)

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './main.scss'
+/*
+    we want to test your expirience with es6 and array manipulation
+    so let's make a deck of cards class with es6 
+        - (52 cards in total)
+        - this deck should contain 4 suits (heart, diamond, spade, club)
+        - each suit to contain 13 cards (ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king)
+    
+    this deck should have the following methods
+
+        - .shuffle()
+            shuffle the deck (sounds like a fun algorithm, but instead show us your npm skills
+            and import a module to help you for this method)
+
+        - .getAllOf( suit )
+            reaveal cards of a chosen suit
+
+        - .deal( number )
+            deal us a card, calling this multiple times should deal us the next card
+            
+        - .compare()
+            compre the top two cards and alert which has a higher value
+
+        tips, extra credit:
+        - using the deck class, how would i go about dynamically adding new suits to the deck
+        - what if i want aces to be high value, or aces to be low value?
+        - can you think of a way to not mutate the deck when dealing cards
+
+    DONT WORRY IF YOU DONT FINISH EVERYTHING!!! :)
+*/
+
+
+// something to get you started:
+class Deck {
+    constructor() {
+
+    }
+}
